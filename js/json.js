b6e1bcde336283315db0ecbfc6348267218bd9fe
BartoszJastrzębski <bartus.jastrzebski@gmail.com>
2017-09-13 21:53:48 +0200

    finito2

--- js/json.js
+++ js/json.js
@@ -1,12 +1,5 @@
 'use strict';
-// funkcja jQuery GETJSON - zwraca nam dane w formacie json
-//$.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function(data) {
-//wyswietl w konsoli
-//    console.log(data);
-//    console.log(data.userId);
-//});
 
-//za pomoca funkcji jQuery ajax (wynik ten sam)
 $.ajax({
     url: "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl",
     dataType: 'json',
